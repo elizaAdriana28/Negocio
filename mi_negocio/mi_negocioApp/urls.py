ce88f2dbfc82c094737b5e67295db81e2a2e833b
elizaAdriana28 <155857618+elizaAdriana28@users.noreply.github.com>
2024-02-09 10:21:32 -0500

    commit eliza

--- mi_negocio/mi_negocioApp/urls.py
+++ mi_negocio/mi_negocioApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from mi_negocioApp import views
 
 urlpatterns = [
-    
+    path('',views.prueba, name="Prueba")    
 
     
 
--- mi_negocio/mi_negocioApp/views.py
+++ mi_negocio/mi_negocioApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def prueba(request):
+    nombre = 'Elizabeth'
+
+    return render(request,'prueba.html',{'name':nombre})
